a6e6de26b5632f63ff2d68de4339231748385d19
Ming Hsieh <xiemingzhi@gmail.com>
2016-07-27 21:45:13 +0800

    First try list view

---
+++ app/src/main/java/com/example/ming/androidlistapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.ming.androidlistapplication;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    static final String[] MOBILE_OS = new String[] { "Android", "iOS",
+            "WindowsMobile", "Blackberry"};
+    static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main,
+                FRUITS));
+
+        ListView listView = getListView();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
