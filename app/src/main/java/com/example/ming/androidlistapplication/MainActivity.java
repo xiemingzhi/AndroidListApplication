ec808bc77ba90f36b74bfa755278ca84eb972a5a
xiemingzhi <xiemingzhi@gmail.com>
2017-03-09 14:30:28 +0800

    Update example for StableArrayAdapter

--- app/src/main/java/com/example/ming/androidlistapplication/MainActivity.java
+++ app/src/main/java/com/example/ming/androidlistapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ming.androidlistapplication;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +12,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ListActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends Activity {
 
     static final String[] MOBILE_OS = new String[] { "Android", "iOS",
             "WindowsMobile", "Blackberry"};
@@ -20,11 +27,16 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main,
-                FRUITS));
+        setContentView(R.layout.activity_main);
+        final ListView listview = (ListView) findViewById(R.id.listview);
 
-        ListView listView = getListView();
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        final ArrayList<String> list = new ArrayList<String>();
+        list.addAll(Arrays.asList(FRUITS));
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 // When clicked, show a toast with the TextView text
@@ -32,5 +44,30 @@ public class MainActivity extends ListActivity {
                         ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
